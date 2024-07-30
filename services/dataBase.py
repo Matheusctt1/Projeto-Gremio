75bd7c5547a6e3bf3c9a3d2c9ccc7d4835ed397a
Matheus Costa da Trindade <135650255+Matheusctt1@users.noreply.github.com>
2024-07-30 15:47:12 -0300

    Update dataBase.py

--- services/dataBase.py
+++ services/dataBase.py
@@ -1,21 +1,21 @@
 import pyodbc as pdb
 import streamlit as st
 
-cnxn = pdb.connect(
-        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
-        + st.secrets["server"]
-        + ";DATABASE="
-        + st.secrets["database"]
-        + ";UID="
-        + st.secrets["username"]
-        + ";PWD="
-        + st.secrets["password"]
-        + ";Trusted_connection=yes;"
-    )
+#cnxn = pdb.connect(
+#        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+#        + st.secrets["server"]
+#        + ";DATABASE="
+#        + st.secrets["database"]
+#        + ";UID="
+#        + st.secrets["username"]
+#        + ";PWD="
+#        + st.secrets["password"]
+#        + ";Trusted_connection=yes;"
+#    )
 
-cursor = cnxn.cursor()
+#cursor = cnxn.cursor()
 
-def init_connection():
-    return cnxn.cursor()
+#def init_connection():
+#    return cnxn.cursor()
 
-conn = init_connection()
+#conn = init_connection()
